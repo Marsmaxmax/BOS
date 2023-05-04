9d1da3eb11a8f98e0660901c7292f7ffd3708163
theendercore <theendercore@gmail.com>
2023-05-04 14:56:07 +0300

    cleaned up& id func

--- src/main/java/com/marsmax/bos/Bos.java
+++ src/main/java/com/marsmax/bos/Bos.java
@@ -1,46 +1,26 @@
 package com.marsmax.bos;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.item.CreativeModeTab;    
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
 
 import com.marsmax.bos.api.CreativeTab;
 import com.marsmax.bos.api.register.RegisterBlock;
-import  com.marsmax.bos.api.register.RegisterItem;
+import com.marsmax.bos.api.register.RegisterItem;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Bos.MODID)
-public class Bos
-{
+public class Bos {
 
     public static final String MODID = "bos";
 
-    public Bos()
-    {
+    public Bos() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         RegisterItem.register(modEventBus);
@@ -51,8 +31,12 @@ public class Bos
         modEventBus.addListener(this::addCreativeTab);
     }
 
+    public static ResourceLocation id(String path) {
+        return new ResourceLocation(MODID, path);
+    }
+
     private void addCreativeTab(CreativeModeTabEvent.BuildContents event) {
-        if(event.getTab() == CreativeTab.BOS_TAB) {
+        if (event.getTab() == CreativeTab.BOS_TAB) {
             //Chips
             event.accept(RegisterItem.ASTEROID_CHIP);
             event.accept(RegisterItem.SATELLITE_CHIP);
@@ -65,8 +49,10 @@ public class Bos
             event.accept(RegisterItem.ITEM_CIRCUIT);
             event.accept(RegisterItem.LIQUID_CIRCUIT);
             event.accept(RegisterItem.CONTR_CIRCUIT);
+
             //Rocket Blocks
             event.accept(RegisterBlock.LAUNCHPAD);
+
             //Armor
             event.accept(RegisterItem.SPACESUIT_HELMET);
             event.accept(RegisterItem.SPACESUIT_CHESTPLATE);
@@ -77,25 +63,29 @@ public class Bos
             event.accept(RegisterItem.NETHER_SPACESUIT_LEGGINGS);
             event.accept(RegisterItem.NETHER_SPACESUIT_BOOTS);
         }
-    
-        if(event.getTab() == CreativeTab.BOS_RES_TAB) {
+
+        if (event.getTab() == CreativeTab.BOS_RES_TAB) {
+
             //Dilithium
             event.accept(RegisterItem.DILITHIUM_CRYSTAL);
             event.accept(RegisterItem.DILITIUM_DUST);
             event.accept(RegisterBlock.DILITHIUM_ORE);
             event.accept(RegisterBlock.DILITHIUM_BLOCK);
+
             //Iridium
             event.accept(RegisterItem.IRIDIUM_RAW);
             event.accept(RegisterItem.IRIDIUM_INGOT);
             event.accept(RegisterBlock.IRIDIUM_ORE);
             event.accept(RegisterBlock.IRIDIUM_BLOCK);
             event.accept(RegisterBlock.IRIDIUM_COIL);
+
             //Tin
             event.accept(RegisterItem.TIN_RAW);
             event.accept(RegisterItem.TIN_INGOT);
             event.accept(RegisterBlock.TIN_ORE);
             event.accept(RegisterBlock.TIN_BLOCK);
             event.accept(RegisterBlock.TIN_COIL);
+
             //Titanium
             event.accept(RegisterItem.TITANIUM_RAW);
             event.accept(RegisterItem.TITANIUM_INGOT);
@@ -105,6 +95,7 @@ public class Bos
 
         }
     }
+
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
