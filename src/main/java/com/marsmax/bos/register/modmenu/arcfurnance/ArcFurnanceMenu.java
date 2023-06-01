e45ae983ce21377c93b4351f6ad45600e46bd141
Marsmaxmax <winterrmaxl@gmail.com>
2023-06-01 17:30:17 +0200

    ArcFurnance Done

--- src/main/java/com/marsmax/bos/register/modmenu/arcfurnance/ArcFurnanceMenu.java
+++ src/main/java/com/marsmax/bos/register/modmenu/arcfurnance/ArcFurnanceMenu.java
@@ -59,7 +59,7 @@ public class ArcFurnanceMenu extends AbstractContainerMenu {
     public int getScaledProgress() {
         int progress = this.data.get(0);
         int maxProgress = this.data.get(1);  // Max Progress
-        int progressArrowSize = 26; // This is the height in pixels of your arrow
+        int progressArrowSize = 27; // This is the height in pixels of your arrow
 
         return maxProgress != 0 && progress != 0 ? progress * progressArrowSize / maxProgress : 0;
     }
--- src/main/java/com/marsmax/bos/register/modmenu/arcfurnance/ArcFurnanceScreen.java
+++ src/main/java/com/marsmax/bos/register/modmenu/arcfurnance/ArcFurnanceScreen.java
@@ -65,7 +65,7 @@ public class ArcFurnanceScreen extends AbstractContainerScreen<ArcFurnanceMenu> 
     //TODO: fix graphical shit
     private void renderProgressArrow(PoseStack pPoseStack, int x, int y) {
         if(menu.isCrafting()) {
-            blit(pPoseStack, x + 105, y + 168, 176, 0, 8, menu.getScaledProgress());
+            blit(pPoseStack, x + 32 , y + 31, 0, 168, 112, menu.getScaledProgress());
         }
     }
 
